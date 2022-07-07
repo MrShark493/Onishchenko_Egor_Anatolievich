59674c1acf060b7206fcd710535e33448ed4a5ab
Onishchenko Egor <onishchenko2001@gmail.com>
2022-07-07 17:17:00 +0300

    Реализовал проверку ФИО. Осталась пара мелочей, может быть позже реализую

---  hw1/hw1.js
+++  hw1/hw1.js
@@ -1,3 +1,23 @@
-let str_age = "-3";
-console.log(isFinite(str_age));
-console.log(Number(str_age));
+do
+{
+  try++;
+  str_name = ""; str_age = "";
+
+  str_name = prompt("Ваше ФИО", "");
+  str_age = prompt("Ваш возраст", '');
+  isMen = confirm("Ваш пол мужской?");
+  str_name = CutTheStringOff(str_name);
+
+  check_name = (str_name.length < 2)||(isHaveNumber(str_name))||(isHaveSpecialSymbols(str_name));
+
+  check_age = !isFinite(str_age)||(str_age == "")||(str_age < 0);
+}
+while ((check_name || check_age) == true);
+
+//////
+
+//////
+
+let str = "    Egor Onishchenko";
+console.log(str);
+console.log(CutTheStringOff(str));
